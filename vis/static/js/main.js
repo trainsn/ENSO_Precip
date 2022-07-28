e09e5875cdf54191a90d61110a91339e921f063d
Neng Shi <shineng@zju.edu.cn>
2022-07-28 14:41:00 -0400

    update index.html layout

--- vis/static/js/main.js
+++ vis/static/js/main.js
@@ -1,174 +1,38 @@
 var parameters =  {"OmM": .12, "OmB": .0215, "h": .55, "theta": 0., "phi": 0.};
 var chart0, chart1, chart2;
 
-var canvas = $("#image")["0"];
+var canvas = $("#precip")["0"];
 var ctx = canvas.getContext("2d");
 
-function init() {
-  // init sliders
-  var sim_slider0_data = [.120, .127, .134, .141, .148, .155];
-  init_slider("#sim_slider0", "OmM", sim_slider0_data, ".3", .120);
-  var sim_slider1_data = [.0215, .0220, .0225, .0230, .0235];
-  init_slider("#sim_slider1", "OmB", sim_slider1_data, ".4", .0215);
-  var sim_slider2_data = [.55, .60, .65, .70, .75, .80, .85];
-  init_slider("#sim_slider2", "h", sim_slider2_data, ".2", .55);
+var time_idx;
 
-  var vis_slider0_data = [0., 60., 120., 180., 240., 300., 360.];
-  init_slider("#vis_slider0", "theta", vis_slider0_data, ",d", 0.);
-  var vis_slider1_data = [-90., -60., -30., 0., 30., 60., 90.];
-  init_slider("#vis_slider1", "phi", vis_slider1_data, ",d", 0.);
-
-  // init line charts
-  chart0 = c3.generate({
-      bindto: "#sim_sens0",
-      data: {
-        columns: [
-          ["data0", 0, 0],
-        ]
-      },
-      axis: {
-        x: {show:false},
-        y: {
-          tick: {
-            format: function (y) { return Math.round(y); }
-          },
-          label: "sensitivity"
-        }
-      },
-      legend: {
-        show: false
-      },
-      size: {
-        height: 100,
-        width: 500
-      },
-      padding: {
-        left: 80,
-        bottom: 0
-      },
-      point: {
-        show: false
-      }
-  });
-
-  chart1 = c3.generate({
-      bindto: '#sim_sens1',
-      data: {
-        columns: [
-          ['data1', 0, 0],
-        ]
-      },
-      axis: {
-        x: {show:false},
-        y: {
-          tick: {
-            format: function (y) { return Math.round(y); }
-          },
-          label: "sensitivity"
-        }
-      },
-      legend: {
-        show: false
-      },
-      size: {
-        height: 100,
-        width: 500
-      },
-      padding: {
-        left: 80,
-        bottom: 0
-      },
-      point: {
-        show: false
-      }
-  });
-
-  chart2 = c3.generate({
-      bindto: '#sim_sens2',
-      data: {
-        columns: [
-          ['data2', 0, 0],
-        ]
-      },
-      axis: {
-        x: {show:false},
-        y: {
-          tick: {
-            format: function (y) { return Math.round(y); }
-          },
-          label: "sensitivity"
-        }
-      },
-      legend: {
-        show: false
-      },
-      size: {
-        height: 100,
-        width: 500
-      },
-      padding: {
-        left: 80,
-        bottom: 0
-      },
-      point: {
-        show: false
-      }
-  });
-}
-
-function init_slider(id, name, data, format, value) {
-  var slider = d3
-    .sliderBottom()
-    .min(d3.min(data))
-    .max(d3.max(data))
-    .width(400)
-    .tickFormat(d3.format(format))
-    .tickValues(data)
-    .default(value)
-    .fill("#a3d4fa")
-    .on("onchange", val => {
-      parameters[name] = val;
-
-      $.ajax({
-        url: "/update",
-        data: parameters,
+canvas.addEventListener("click", function __handler__(evt) {
+    var lon_idx = evt.clientX;
+    var lat_idx = evt.clientY;
+    var rect = canvas.getBoundingClientRect();
+    lon_idx -= rect.left;
+    lat_idx -= rect.top;
+    lon_idx = parseInt(lon_idx / 2);
+    lat_idx = parseInt((canvas.height - 1 - lat_idx) / 2);
+    time_spatial_indices = {"time_idx": time_idx["idx"], "lat_idx": lat_idx, "lon_idx": lon_idx};
+    $.ajax({
+        url: "/backward_prop",
+        data: time_spatial_indices,
         type: "POST",
-        success: function(response) {
-          var canv = $("#image")["0"];
-          kd.utils.draw_img_clr(canv, response.image, 2);
-          var ctx = canv.getContext("2d");
-        },
         error: function(error) {
-          console.log(error);
+            console.log(error);
         }
-      });
-    });
-
-  var slider_g = d3
-    .select(id)
-    .append("svg")
-    .attr("width", 520)
-    .attr("height", 60)
-    .append("g")
-    .attr("transform", "translate(30,20)");
-
-  slider_g.append("text")
-    .style("font-size", "16px")
-    .attr("transform", "translate(0,4)")
-    .text(name);
-  slider_g.append("g")
-    .attr("transform", "translate(60,0)")
-    .call(slider);
-}
+  });
+});
 
 function update_month(selectObject) {
-  var month_idx = {"idx": selectObject.selectedIndex - 1}
+  time_idx = {"idx": selectObject.selectedIndex - 1};
   $.ajax({
-    url: "/update_month",
-    data: month_idx,
+    url: "/forward_prop",
+    data: time_idx,
     type: "POST",
     success: function(response) {
-      var canvas = $("#image")["0"];
+      var canvas = $("#precip")["0"];
       var ctx = canvas.getContext("2d");
 
       var imgData = ctx.createImageData(canvas.width, canvas.height); // width x height
@@ -177,10 +41,10 @@ function update_month(selectObject) {
       // copy img byte-per-byte into our ImageData
       for (var i = 0; i < canvas.height; i++) {
         for (var j = 0; j < canvas.width; j++){
-            data[(i * canvas.width + j) * 4] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4]
-            data[(i * canvas.width + j) * 4 + 1] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 1]
-            data[(i * canvas.width + j) * 4 + 2] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 2]
-            data[(i * canvas.width + j) * 4 + 3] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 3]
+            data[(i * canvas.width + j) * 4] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4];
+            data[(i * canvas.width + j) * 4 + 1] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 1];
+            data[(i * canvas.width + j) * 4 + 2] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 2];
+            data[(i * canvas.width + j) * 4 + 3] = response.image[((canvas.height - 1 - i) * canvas.width + j) * 4 + 3];
         }
       }
 
